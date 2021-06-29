c6c75ef288916fa001cc620ef83d2b9f654a1d17
Joel Ekne <joel.ekne@skymill.io>
2021-06-29 17:18:01 +0200

    more test

--- Controller.java
+++ Controller.java
@@ -5,7 +5,7 @@ import java.io.ObjectOutputStream;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-//test
+//testING STUFF BRO
 
 public class Controller {
     List<Activity> activityList = new ArrayList<>();
